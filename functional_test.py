cfe8abcf782797d1ce0b6002baa05a4f493f0ac2
Alex Baker <al3xb4k3r@gmail.com>
2014-11-17 11:51:18 -0700

    add app for lists, with deliberately failing list

--- functional_test.py
+++ functional_test.py
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test!')
 
-        # She id invited to enter a to-do item straight away
+        # She is invited to enter a to-do item straight away
 
         # She types "Buy peacock feathers: into a text box
         # (Edith's hobby is tying fly-fishing lures)
