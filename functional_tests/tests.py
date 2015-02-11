0d75348b967c8df2d3e2cc015e3ad8b34b569159
Alex Baker <al3xb4k3r@gmail.com>
2015-02-11 10:47:18 -0700

    fixed incorrect variable name

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -109,7 +109,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.server_ur)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
